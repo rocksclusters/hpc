be8f490c5ea5e779d3ab0d9653c02d486b879905
Anoop Rajendra <anoop.rajendra@gmail.com>
2008-07-28 16:04:14 +0000

    Added code to be able to download the sun-cluster tools and sun-studio tools during build.

--- src/sunos.mk
+++ src/sunos.mk
@@ -1,2 +1,13 @@
 SRCDIRS = iozone iperf mpi-tests \
-	  stream PKGS
+	  stream sun-ct
+pkg::
+	if [ -d sun-ct ]; then	\
+		(cd sun-ct; hg pull; hg update); \
+	else \
+		hg clone ssh://hg@surya.rocksclusters.org/sun-utils/sun-ct;\
+	fi
+
+.PHONY: sun-ct
+clean:: sun-ct
+	-(cd sun-ct; gmake clean)
+
