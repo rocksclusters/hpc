b8726bd627268bf9375a68c51d87fadae7b6da75
Anoop Rajendra <anoop.rajendra@gmail.com>
2008-12-17 00:22:38 +0000

    Cleaned up mercurial retrieval for sun cluster tools. Now if downloading fails, roll building won't fail

--- src/sunos.mk
+++ src/sunos.mk
@@ -1,13 +1,21 @@
 SRCDIRS = iozone iperf mpi-tests \
-	  stream sun-ct
-pkg::
-	if [ -d sun-ct ]; then	\
-		(cd sun-ct; hg pull; hg update); \
+	  stream
+
+HG_SOURCE = ssh://hg@rocks-168.sdsc.edu/sun-utils
+HG_PACKAGE= sun-ct
+
+$(HG_PACKAGE)::
+	-if [ -d $@ ]; then	\
+		(cd $@;\
+		hg pull $(HG_SOURCE)/$@ ; \
+		hg update;		\
+	); \
 	else \
-		hg clone ssh://hg@aurora.rocksclusters.org/sun-utils/sun-ct;\
+		hg clone $(HG_SOURCE)/$@;\
 	fi
 
-.PHONY: sun-ct
-clean:: sun-ct
-	-(cd sun-ct; gmake clean)
+pkg:: $(HG_PACKAGE)
 
+pkg clean::
+PKG_EXISTS = $(shell [ -d $(dir) ] && echo $(dir))
+SRC_DIRS += $(foreach dir, $(HG_PACKAGE), $(PKG_EXISTS))
