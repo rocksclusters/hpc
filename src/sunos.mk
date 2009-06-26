3e949ff8f781fbee1cc6fc78abf39bc7af63afdb
Anoop Rajendra <anoop.rajendra@gmail.com>
2009-06-26 00:57:56 +0000

    added sun-hpc cluster tools to the hpc roll

--- src/sunos.mk
+++ src/sunos.mk
@@ -1,21 +1,2 @@
 SRCDIRS = iozone iperf mpi-tests \
-	  stream
-
-HG_SOURCE = ssh://hg@rocks-168.sdsc.edu/sun-utils
-HG_PACKAGE= sun-ct
-
-$(HG_PACKAGE)::
-	-if [ -d $@ ]; then	\
-		(cd $@;\
-		hg pull $(HG_SOURCE)/$@ ; \
-		hg update;		\
-	); \
-	else \
-		hg clone $(HG_SOURCE)/$@;\
-	fi
-
-pkg:: $(HG_PACKAGE)
-
-pkg clean::
-PKG_EXISTS = $(shell [ -d $(dir) ] && echo $(dir))
-SRCDIRS += $(foreach dir, $(HG_PACKAGE), $(PKG_EXISTS))
+	  stream sun-ct
