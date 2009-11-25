88275a28f334324b28db2288d2648c3595e98f10
Anoop Rajendra <anoop.rajendra@gmail.com>
2009-11-25 23:34:24 +0000

    Upgraded to latest Sun HPC Cluster tools for SunOS Added patch for Sun HPC Cluster tools so that installation won't fail

--- src/sun-ct/sunos.mk
+++ src/sun-ct/sunos.mk
@@ -4,4 +4,12 @@ SHAREDIR = sun/share
 
 install::
 	if [ ! -d $(REDHAT.ROOT)/PKGS/ ]; then mkdir -p $(REDHAT.ROOT)/PKGS/; fi
+	# Patch the pkgmap files to use group "bin" and not "hpcgroup"
+	( for i in `find $(ARCHIVENAME)-$(VERSION)-$(RELEASE)/Product \
+			-type f -name pkgmap`; \
+		do						\
+		cat $$i | sed 's/hpcgroup/bin/g' > $${i}.new;	\
+		mv $${i}.new $$i;				\
+		done;						\
+	)
 	pkgtrans $(ARCHIVENAME)-$(VERSION)-$(RELEASE)/Product $(REDHAT.ROOT)/PKGS/ all
