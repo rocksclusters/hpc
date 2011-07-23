afa9372f5270826e3edb9f7fb77e9ee309b466b6
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2011-07-23 02:31:02 +0000

    Viper Copyright

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 #
 # This file should remain OS independent
 #
-# $Id: bootstrap.sh,v 1.12 2010/09/07 23:53:20 bruno Exp $
+# $Id: bootstrap.sh,v 1.13 2011/07/23 02:31:02 phil Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		         version 5.4 (Maverick)
+# 		         version 5.4.3 (Viper)
 # 
-# Copyright (c) 2000 - 2010 The Regents of the University of California.
+# Copyright (c) 2000 - 2011 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
 # features or use of this software must display the following acknowledgement: 
 # 
 # 	"This product includes software developed by the Rocks(r)
-# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	Development Team at the San Diego Supercomputer Center at the
 # 	University of California, San Diego and its contributors."
 # 
 # 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
@@ -58,6 +58,9 @@
 # @Copyright@
 #
 # $Log: bootstrap.sh,v $
+# Revision 1.13  2011/07/23 02:31:02  phil
+# Viper Copyright
+#
 # Revision 1.12  2010/09/07 23:53:20  bruno
 # star power for gb
 #
