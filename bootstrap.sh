0ad939681f6c1b080f1381c9b0049802039cee03
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-11-27 00:49:00 +0000

    Copyright Storm for Emerald Boa

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,16 @@
 #
 # This file should remain OS independent
 #
-# $Id: bootstrap.sh,v 1.15 2012/09/30 21:58:55 phil Exp $
+# $Id: bootstrap.sh,v 1.16 2012/11/27 00:49:00 phil Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		         version 5.5 (Mamba)
-# 		         version 6.0 (Mamba)
+# 		         version 5.6 (Emerald Boa)
+# 		         version 6.1 (Emerald Boa)
 # 
-# Copyright (c) 2000 - 2012 The Regents of the University of California.
+# Copyright (c) 2000 - 2013 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -59,6 +59,9 @@
 # @Copyright@
 #
 # $Log: bootstrap.sh,v $
+# Revision 1.16  2012/11/27 00:49:00  phil
+# Copyright Storm for Emerald Boa
+#
 # Revision 1.15  2012/09/30 21:58:55  phil
 # Update openmpi to 1.6.2.
 # Create Modules for ethernet and IB
--- include/installclass/ia64.py
+++ include/installclass/ia64.py
@@ -1,13 +1,13 @@
-# $Id: ia64.py,v 1.18 2012/05/06 05:49:09 phil Exp $
+# $Id: ia64.py,v 1.19 2012/11/27 00:49:00 phil Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		         version 5.5 (Mamba)
-# 		         version 6.0 (Mamba)
+# 		         version 5.6 (Emerald Boa)
+# 		         version 6.1 (Emerald Boa)
 # 
-# Copyright (c) 2000 - 2012 The Regents of the University of California.
+# Copyright (c) 2000 - 2013 The Regents of the University of California.
 # All rights reserved.	
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -55,6 +55,9 @@
 # @Copyright@
 #
 # $Log: ia64.py,v $
+# Revision 1.19  2012/11/27 00:49:00  phil
+# Copyright Storm for Emerald Boa
+#
 # Revision 1.18  2012/05/06 05:49:09  phil
 # Copyright Storm for Mamba
 #
