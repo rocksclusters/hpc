4333c066a18a34e42d00b56460114d956c523d6c
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2015-01-14 12:08:48 -0800

    Change name of iperf package to opt-iperf. Eliminates conflict with epel version

--- src/iperf/version.mk
+++ src/iperf/version.mk
@@ -1,5 +1,6 @@
-NAME		= iperf
+NAME		= opt-iperf
 VERSION		= 2.0.5
 RELEASE		= 1
 PKGROOT		= /opt/iperf
 COPYRIGHT	= The Board of Trustees of the University of Illinois
+TARNAME		= iperf-$(VERSION).tar.gz
