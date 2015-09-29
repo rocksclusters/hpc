15b7f96a81180c95ec2b92d80a3082af6b585498
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2015-09-29 12:54:21 -0700

    Update openmpi version to 1.10.0

--- src/openmpi/version.mk
+++ src/openmpi/version.mk
@@ -1,3 +1,13 @@
 NAME		= rocks-openmpi
-VERSION		= 1.6.2
+VERSION		= 1.10.0
 RELEASE		= 1
+
+TARBALL-EXTENSION = tar.gz
+#### if a bzip2 file
+## TARBALL-EXTENSION = tar.bz2
+
+ifneq (,$(findstring bz2, $(TARBALL-EXTENSION)))
+CAT-COMPRESS = bzcat
+else
+CAT-COMPRESS = zcat
+endif
