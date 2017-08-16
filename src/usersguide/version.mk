92e921005b915d2eab4128233bbdf0a44b04a6dd
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-16 13:23:58 -0700

    RPM.FILES for users guide. remove opensm-libs package on 7 (no longer packaged that way)

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,6 +1,7 @@
 NAME    = roll-$(ROLL)-usersguide
 RELEASE = 0
 RPM.ARCH = noarch
+PKGROOT         = /var/www/html/roll-documentation/hpc/$(VERSION)
 
 SUMMARY_COMPATIBLE	= $(VERSION)
 SUMMARY_MAINTAINER	= Rocks Group
@@ -9,3 +10,4 @@ SUMMARY_ARCHITECTURE	= i386, x86_64
 ROLL_REQUIRES		= base kernel os
 ROLL_CONFLICTS		=
 
+RPM.FILES = $(PKGROOT) 
